e0c6db7002e6580395c94e9d262721a82a90bc57
Srinivas <chebolu572@gmail.com>
2025-08-24 15:20:17 +0530

    Added ExtentReports and Updated Hooks

--- Hooks/HooksTester.cs
+++ Hooks/HooksTester.cs
@@ -1,39 +1,130 @@
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Reqnroll;
 using Reqnroll.BoDi;
+using ReqnRollBDD.Utility;
 
 namespace ReqnRollBDD.Hooks
 {
     [Binding]
-    public sealed class HooksTester
+    public sealed class HooksTester : ExtentReport
     {
         private readonly IObjectContainer _objectContainer;
         public HooksTester(IObjectContainer container)
         {
             _objectContainer = container;
         }
-        [BeforeScenario("@TestersTalk")]
+        public static ExtentReports _extentReports;
+
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            Console.WriteLine("Running before test run...");
+            ExtentReportInit();
+        }
+
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            Console.WriteLine("Running after test run...");
+            ExtentReportTearDown();
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
+        {
+            Console.WriteLine("Running before feature...");
+            _feature = _extentReports.CreateTest<Feature>(featureContext.FeatureInfo.Title);
+        }
+
+        [AfterFeature]
+        public static void AfterFeature()
+        {
+            Console.WriteLine("Running after feature...");
+        }
+
+        [BeforeScenario("@Testers")]
         public void BeforeScenarioWithTag()
         {
-            Console.WriteLine("This will run before the scenario with @TestersTalk tag");
+            Console.WriteLine("Running inside tagged hooks in specflow");
         }
 
         [BeforeScenario(Order = 1)]
-        public void FirstBeforeScenario()
+        public void FirstBeforeScenario(ScenarioContext scenarioContext)
         {
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             _objectContainer.RegisterInstanceAs<IWebDriver>(driver);
+            _scenario = _feature.CreateNode<Scenario>(scenarioContext.ScenarioInfo.Title);
         }
 
         [AfterScenario]
         public void AfterScenario()
         {
-           var driver= _objectContainer.Resolve<IWebDriver>();
-            if(driver != null)
+            var driver = _objectContainer.Resolve<IWebDriver>();
+            if (driver != null)
             {
                 driver.Quit();
+            }
+        }
+        [AfterStep]
+        public void AfterStep(ScenarioContext scenarioContext)
+        {
+            Console.WriteLine("Running after step....");
+            string stepType = scenarioContext.StepContext.StepInfo.StepDefinitionType.ToString();
+            string stepName = scenarioContext.StepContext.StepInfo.Text;
+
+            var driver = _objectContainer.Resolve<IWebDriver>();
+
+            //When scenario passed
+            if (scenarioContext.TestError == null)
+            {
+                if (stepType == "Given")
+                {
+                    _scenario.CreateNode<Given>(stepName);
+                }
+                else if (stepType == "When")
+                {
+                    _scenario.CreateNode<When>(stepName);
+                }
+                else if (stepType == "Then")
+                {
+                    _scenario.CreateNode<Then>(stepName);
+                }
+                else if (stepType == "And")
+                {
+                    _scenario.CreateNode<And>(stepName);
+                }
+            }
+
+            //When scenario fails
+            if (scenarioContext.TestError != null)
+            {
+
+                if (stepType == "Given")
+                {
+                    _scenario.CreateNode<Given>(stepName).Fail(scenarioContext.TestError.Message,
+                        MediaEntityBuilder.CreateScreenCaptureFromPath(AddScreenshot(driver, scenarioContext)).Build());
+                }
+                else if (stepType == "When")
+                {
+                    _scenario.CreateNode<When>(stepName).Fail(scenarioContext.TestError.Message,
+                        MediaEntityBuilder.CreateScreenCaptureFromPath(AddScreenshot(driver, scenarioContext)).Build());
+                }
+                else if (stepType == "Then")
+                {
+                    _scenario.CreateNode<Then>(stepName).Fail(scenarioContext.TestError.Message,
+                        MediaEntityBuilder.CreateScreenCaptureFromPath(AddScreenshot(driver, scenarioContext)).Build());
+                }
+                else if (stepType == "And")
+                {
+                    _scenario.CreateNode<And>(stepName).Fail(scenarioContext.TestError.Message,
+                        MediaEntityBuilder.CreateScreenCaptureFromPath(AddScreenshot(driver, scenarioContext)).Build());
+                }
             }
         }
     }
---
+++ Utility/ExtentReport.cs
@@ -0,0 +1,57 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Config;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ReqnRollBDD.Utility
+{
+    public class ExtentReport
+    {
+        public static ExtentReports _extentReports;
+        public static ExtentTest _feature;
+        public static ExtentTest _scenario;
+
+        public static String dir = AppDomain.CurrentDomain.BaseDirectory;
+        public static String testResultPath = dir.Replace("bin\\Debug\\net6.0", "TestResults");
+
+        public static void ExtentReportInit()
+        {
+            var htmlReporter = new ExtentSparkReporter(testResultPath);
+            htmlReporter.Config.ReportName = "Automation Status Report";
+            htmlReporter.Config.DocumentTitle = "Automation Status Report";
+            htmlReporter.Config.Theme = Theme.Standard;
+          
+            _extentReports = new ExtentReports();
+            _extentReports.AttachReporter(htmlReporter);
+            _extentReports.AddSystemInfo("Application", "Youtube");
+            _extentReports.AddSystemInfo("Browser", "Chrome");
+            _extentReports.AddSystemInfo("OS", "Windows");
+        }
+
+        public static void ExtentReportTearDown()
+        {
+            _extentReports.Flush();
+        }
+
+        public string AddScreenshot(IWebDriver driver, ScenarioContext scenarioContext)
+        {
+            ITakesScreenshot takesScreenshot = (ITakesScreenshot)driver;
+            Screenshot screenshot = takesScreenshot.GetScreenshot();
+
+            string fileName = $"{scenarioContext.ScenarioInfo.Title}_{DateTime.Now:yyyyMMdd_HHmmss}.png";
+            string screenshotLocation = Path.Combine(testResultPath, fileName);
+
+            // ✅ Use extension-based saving (no need for ScreenshotImageFormat)
+            screenshot.SaveAsFile(screenshotLocation);
+
+            return screenshotLocation;
+        }
+
+
+    }
+}
+
